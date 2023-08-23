53841f94d2022a20fccc932bd01788e980f224f7
Radu Petrisel <radupetrisel@gmail.com>
2023-08-23 17:26:16 +0300

    Initial Commit

---
+++ Radu Petrisel iOS Developer/Radu_Petrisel_iOS_DeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radu_Petrisel_iOS_DeveloperApp.swift
+//  Radu Petrisel iOS Developer
+//
+//  Created by Radu Petrisel on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Radu_Petrisel_iOS_DeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
